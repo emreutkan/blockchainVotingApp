d30defc4c2133c72add558b9bd6e97622debba0f
emreutkan <127414322+emreutkan@users.noreply.github.com>
2024-12-20 23:05:35 +0300

    checkpoint

--- setup.bat
+++ setup.bat
@@ -16,4 +16,7 @@ REM Generate requirements.txt
 pip freeze > requirements.txt
 
 echo Virtual environment setup complete and activated.
-pause
+echo You can now start the app by typing
+echo python app.py --port=5001 (Note: Replace 5001 with the desired port number for each node (5002, 5003, etc.).)
+REM start the app
+python app.py --port=5001
